fb8be08e4c78995e28e648334de724683a5f0811
Cole Douglas <cole@pivotalprint.co.nz>
2026-06-04 16:37:18 +1200

    final scientist description

--- docs/assets/app.js
+++ docs/assets/app.js
@@ -2,7 +2,7 @@
 const roles = [
     { name: "Bodyguard", order: 1, instructions: "Place a token of protection on a player's card. This card cannot be tampered with tonight." },
     { name: "Captain", order: 2, instructions: "Look at a player's card and swap it with a card from the centre of the opposite colour." },
-    { name: "Scientist", order: 3, instructions: "TEST TEST TEST" },
+    { name: "Scientist", order: 3, instructions: "Look at another player's card to learn their identity. Then return it face down." },
     { name: "Technician", order: 4, instructions: "Flip a face down card face up and a face up card face down." },
     { name: "Saboteur", order: 5, instructions: "Flip over another player's card, revealing their identity to everyone." },
 ];
